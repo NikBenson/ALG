dbd942915f134cc0af7469f84b2d7368ad693621
Nik Benson <nbenson@nettrek.de>
2022-05-10 21:29:53 +0200

    continued review

--- code/src/samplesort/select_splitters.c
+++ code/src/samplesort/select_splitters.c
@@ -1,11 +1,11 @@
 #include "select_splitters.h"
 
 Array selectSplitters(Array data, SampleSortConfiguration configuration) {
-    Array sample = configuration.sampler(data, configuration.oversamplingFactor * configuration.splittersCount, configuration.swap);
+    Array sample = configuration.sampler(data, configuration.oversamplingFactor * (configuration.splittersCount-1)+1, configuration.swap);
     configuration.smallSort(sample, configuration.compare, configuration.swap);
 
     unsigned int sampleLength = arrayLength(sample);
-    unsigned int steps = sampleLength / configuration.splittersCount;
+    unsigned int steps = (sampleLength-1) / (configuration.splittersCount-1);
 
     for (int i = 0; i < configuration.splittersCount; ++i) {
         int *targetPosition = sample.start + i;
